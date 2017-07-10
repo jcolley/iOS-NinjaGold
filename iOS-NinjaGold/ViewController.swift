f21f3e214819372c00542d5911a9671c2cd47223
John Colley <webjocky@gmail.com>
2017-07-10 14:21:06 -0700

    initial commit

--- iOS-NinjaGold/ViewController.swift
+++ iOS-NinjaGold/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalGold: UILabel!
+    @IBOutlet weak var farmGoldAdded: UILabel!
+    @IBOutlet weak var caveGoldAdded: UILabel!
+    @IBOutlet weak var houseGoldAdded: UILabel!
+    @IBOutlet weak var casinoGoldAdded: UILabel!
+    
+    var goldTotal: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,69 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func bldgButtonPressed(_ sender: UIButton) {
+        // Loop through all labels each time bldg button is pressed and hide all (except Gold and Total Gold, tagged with 1)
+        hideLabels()
+        
+        if sender.tag == 1 {
+            let lowerValue = 10
+            let upperValue = 50
+            let gold = Int(arc4random_uniform(UInt32(upperValue - lowerValue + 1))) + lowerValue
+            goldTotal += gold
+            updateTotal(gold: goldTotal)
+            farmGoldAdded.text = "You earned \(gold)"
+            farmGoldAdded.isHidden = false
+        } else if sender.tag == 2 {
+            let lowerValue = 5
+            let upperValue = 10
+            let gold = Int(arc4random_uniform(UInt32(upperValue - lowerValue + 1))) + lowerValue
+            goldTotal += gold
+            updateTotal(gold: goldTotal)
+            caveGoldAdded.text = "You earned \(gold)"
+            caveGoldAdded.isHidden = false
+        } else if sender.tag == 3 {
+            let lowerValue = 2
+            let upperValue = 5
+            let gold = Int(arc4random_uniform(UInt32(upperValue - lowerValue + 1))) + lowerValue
+            goldTotal += gold
+            updateTotal(gold: goldTotal)
+            houseGoldAdded.text = "You earned \(gold)"
+            houseGoldAdded.isHidden = false
+        } else if sender.tag == 4 {
+            let lowerValue = -50
+            let upperValue = 50
+            let gold = Int(arc4random_uniform(UInt32(upperValue - lowerValue + 1))) + lowerValue
+            goldTotal += gold
+            updateTotal(gold: goldTotal)
+            var wiggle = ""
+            if gold < 0 {
+                wiggle = "lost"
+            } else if gold >= 0 {
+                wiggle = "earned"
+            }
+            casinoGoldAdded.text = "You \(wiggle) \(gold)"
+            casinoGoldAdded.isHidden = false
+        }
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        goldTotal = 0
+        updateTotal(gold: goldTotal)
+        hideLabels()
+    }
+    
+    func updateTotal(gold: Int){
+        totalGold.text = String(gold)
+    }
+    
+    func hideLabels(){
+        for view in self.view.subviews as [UIView] {
+            if let lbl = view as? UILabel {
+                if lbl.tag != 1{
+                    lbl.isHidden = true
+                }
+            }
+        }
+    }
 }
 
